e237196b0e3096850fa2d38380d286d577e7e369
Aaron Thompson <47483901+backpack5689@users.noreply.github.com>
2021-04-22 10:14:34 -0500

    Delete target directory
    Deleted this because it can be built by cargo commands locally for now

--- target/debug/cubertcli.d
+++
@@ -1,1 +0,0 @@
-/mnt/c/Users/aaron/Desktop/Projects/cubertcli/target/debug/cubertcli: /mnt/c/Users/aaron/Desktop/Projects/cubertcli/src/main.rs
--- target/debug/deps/cubertcli-72208d327dd2cf15.d
+++
@@ -1,5 +0,0 @@
-/mnt/c/Users/aaron/Desktop/Projects/cubertcli/target/debug/deps/cubertcli-72208d327dd2cf15: src/main.rs
-
-/mnt/c/Users/aaron/Desktop/Projects/cubertcli/target/debug/deps/cubertcli-72208d327dd2cf15.d: src/main.rs
-
-src/main.rs:
